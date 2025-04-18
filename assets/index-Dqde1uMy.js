d3d15a26bfacdec3a7f042b755742d761499a9d9
马叉虫 <zhengzhiliangzzl@163.com>
2025-04-19 01:20:07 +0800

    Add files via upload

---
+++ assets/index-Dqde1uMy.js
@@ -0,0 +1,1 @@
+import{d as t,az as r,ay as a,c as o,o as s}from"./vue-BIr_Kbek.js";const m=t({__name:"index",setup(n){const e=a();return r().replace({path:`/${e.params.path}`,query:e.query}),(c,p)=>(s(),o("div"))}});export{m as default};
